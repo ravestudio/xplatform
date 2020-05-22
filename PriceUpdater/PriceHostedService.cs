dee0d2563d1abe8255babeae46d4409499ef3453
Станислав <RaveStudio@mail.ru>
2020-05-22 10:31:24 +0600

    reome console

--- PriceUpdater/PriceHostedService.cs
+++ PriceUpdater/PriceHostedService.cs
@@ -66,7 +66,7 @@ namespace PriceUpdater
                 subscription = quoteSeq.Subscribe(q =>
                 {
 
-                    Console.WriteLine($"{q.symbol}: {q.price}");
+                    //Console.WriteLine($"{q.symbol}: {q.price}");
                 },
                 ex =>
                 {
