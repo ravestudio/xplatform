821902846ef2775ad4e036508aacf1cda3b0fa58
Yakovlev Stanislav <ravestudio@mail.ru>
2020-06-04 13:23:31 +0600

    .

--- xplatform/Program.cs
+++ xplatform/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace xplatform
 {
@@ -14,11 +16,23 @@ namespace xplatform
     {
         public static void Main(string[] args)
         {
+            string logPath = Environment.GetEnvironmentVariable("logPath");
+            string logFile = Path.Combine(logPath, "xplatform.txt");
+
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.File(logFile, rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
