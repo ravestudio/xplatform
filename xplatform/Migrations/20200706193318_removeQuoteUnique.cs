86949d9c7a978798f0c473a3c5aff6f46edfe138
Станислав <RaveStudio@mail.ru>
2020-07-07 01:36:06 +0600

    remove quote key

---
+++ xplatform/Migrations/20200706193318_removeQuoteUnique.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace xplatform.Migrations
+{
+    public partial class removeQuoteUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_QuoteSet",
+                table: "QuoteSet");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Symbol",
+                table: "QuoteSet",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Symbol",
+                table: "QuoteSet",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_QuoteSet",
+                table: "QuoteSet",
+                column: "Symbol");
+        }
+    }
+}
